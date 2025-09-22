682dd38c7a218019a8a9cc0f62161d7325f0194b
pappz <zoltan.papp@hoya.hu>
2025-09-22 09:53:42 +0200

    adding pz to fooder

--- Includes/templates/footer.php
+++ Includes/templates/footer.php
@@ -1,24 +1,35 @@
-		
-		<footer class="footer_section">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6 xs-padding">
-						<div class="copyright">
-							© 
-							<script type="text/javascript"> 
-								document.write(new Date().getFullYear())
-							</script>
-							BY:P.Z
-						</div>
-					</div>
-					<div class="col-md-6 xs-padding">
-						<ul class="footer_social">
-							
-						</ul>
-					</div>
-				</div>
-			</div>
-		</footer>
+<footer class="footer_section">
+  <div class="text-center">
+    <a href="https://pzoli.com" target="_blank" class="matrix-link">
+      © <script>document.write(new Date().getFullYear())</script> BY: pZoli.com
+    </a>
+  </div>
+</footer>
+
+<style>
+.footer_section {
+  background: transparent !important; /* ne legyen széles háttér */
+  border: none;
+  margin: 0;
+  padding: 5px 0; /* minimális padding felül/alul */
+}
+
+.matrix-link {
+  background: #000;             /* fekete háttér CSAK a szöveg mögött */
+  color: #00ff66;               /* Matrix-zöld szöveg */
+  font-family: 'Courier New', monospace;
+  text-shadow: 0 0 5px #00ff66;
+  text-decoration: none;
+  display: inline;              /* pontosan a szöveg szélességére igazodjon */
+  padding: 2px 6px;             /* nagyon vékony keret a szöveg körül */
+}
+
+.matrix-link:hover {
+  color: #fff;
+  text-shadow: 0 0 10px #00ff66, 0 0 20px #00ff66;
+}
+</style>
+
 
 	
 		<!-- INCLUDE JS SCRIPTS -->
